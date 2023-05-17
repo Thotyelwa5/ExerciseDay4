0c34769ab451f5303641095bcd4b8b1cb08de53b
Thotyelwa <mpongwanathotyelwa@gmail.com>
2023-05-17 14:32:53 +0200

    update

---
+++ script/code.js
@@ -0,0 +1,12 @@
+let person1 = {
+    name: "Thotyelwa",
+    age: 22 ,
+}
+console.log(person1);
+let person2 = {
+    name: "Kamvelihle",
+    age: 20,
+}
+console.log(person2);
+let peopleArray = [person1, person2];
+console.log(peopleArray);
